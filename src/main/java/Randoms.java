49a0ed6d6ea7e92bbfc1443ea9ea5a0bbcc48210
Victor Alyonkin <viktoalenkin@ya.ru>
2022-12-20 17:01:41 +0300

    first commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,41 @@
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private int min, max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt((max - min) + 1) + min;
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return Iterable.super.spliterator();
+    }
+}
